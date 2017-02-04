097c16a97c9ccb4902c5d7b18c203584282e4785
Tarnenak Yauheni <tarnenok@gmail.com>
2017-02-04 11:57:28 +0300

    initial commit

---
+++ RiderExeption/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RiderExeption
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            throw new Exception();
+            Console.Read();
+        }
+    }
+}
---
+++ RiderWebException/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RiderWebException.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new Exception();
+            return new string[] {"value1", "value2"};
+        }
+    }
+}
